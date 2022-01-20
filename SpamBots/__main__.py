620db4d419d02912c02b73185db9f60a89a31dcb
DEFENDERS2022BOY <98003220+DEFENDERS2022BOY@users.noreply.github.com>
2022-01-20 11:26:11 +0530

    Update __main__.py

--- SpamBots/__main__.py
+++ SpamBots/__main__.py
@@ -16,7 +16,7 @@ for name in files:
         load_plugins(plugin_name.replace(".py", ""))
 
 print("Successfully deployed!")
-print("Enjoy! Do visit @LEGACY_MULTI_SPAM_BOT")
+print("Enjoy! Do visit @DEFENDERS_SPAM_BOT")
 
 if __name__ == "__main__":
     UstaD.run_until_disconnected()
